665041e9e2e091891ddfb1d4d11467c9daf5ed7c
Peter Leisen <melis2Melissalaptop.(non)>
2023-11-20 14:58:14 -0500

    figured out mode, partially

--- project_1/src/passcodes.py
+++ project_1/src/passcodes.py
@@ -3,10 +3,9 @@
 
 def passcode_list():
     
-    pwd = input("Create a master password by typing here:  ")
+    #pwd = input("Create a master password by typing here:  ")
 
-    mode = input("Please choose weither to view existing passwords or add a new password:\
-                    (view, add)")
+    mode = input("Please choose weither to view existing passwords or add a new password: (view, add)")
 
     if mode == "view":
         pass

b6858dfa8dbca4897f9d73d2d98830e4af7e72b7
Peter Leisen <melis2Melissalaptop.(non)>
2023-12-02 23:43:48 -0500

    Think Added While Loop with q Break

--- project_1/src/passcodes.py
+++ project_1/src/passcodes.py
@@ -5,15 +5,18 @@ def passcode_list():
     
     #pwd = input("Create a master password by typing here:  ")
 
-    mode = input("Please choose weither to view existing passwords or add a new password: (view, add)")
+    mode = input("Please choose weither to view existing passwords or add a new password: (view, add), press q to quit? ").lower() 
 
-    if mode == "view":
-        pass
-    elif mode == "add":
-        pass
-    else: 
-        print("Invalid mode.")
-
+    while True:
+        if mode == "q":
+            break
+        if mode == "view":
+            pass
+        elif mode == "add":
+            pass
+        else: 
+            print("Invalid mode.")
+            continue
 
     #print("Testing")
 

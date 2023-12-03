2649e4d9ef0417d394732d73a4f553bd18561079
Peter Leisen <melis2Melissalaptop.(non)>
2023-12-03 13:51:59 -0500

    Fixed Break and Made View Function

--- project_1/src/passcodes.py
+++ project_1/src/passcodes.py
@@ -2,7 +2,11 @@
 
 
 def view():
-    pass
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:", passw)
 
 def add():
     name = input('Account Name: ')
@@ -17,11 +21,11 @@ def passcode_list():
     
     #pwd = input("Create a master password by typing here:  ")
 
-    mode = input("Please choose weither to view existing passwords or add a new password: (view, add), press q to quit? ").lower() 
-
     while True:
+        mode = input("Please choose weither to view existing passwords or add a new password: (view, add), press q to quit? ").lower() 
         if mode == "q":
             break
+
         if mode == "view":
             view()
         elif mode == "add":
@@ -29,6 +33,7 @@ def passcode_list():
         else: 
             print("Invalid mode.")
             continue
+            
 
     #print("Testing")
 
